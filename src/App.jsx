5ca9b28d77fae46d76d72d1886efd1ef742f94d2
Gatiskk <gatisk19@inbox.lv>
2022-11-24 11:35:33 +0200

    First

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Giphy from "./components/Giphy"
+import "./App.css"
+
+const App = () => {
+    return <div><Giphy/></div>
+}
+
+export default App
---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loader = () => {
+    return <div className="loader">
+        <i className="fa-solid fa-spinner fa-4x fa-spin"/>
+    </div>
+}
+
+export default Loader
